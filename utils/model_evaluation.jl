3127132d1984b364f6cf797faf336c447d0e47fd
Marcin Jędrzejowski <marcin.jott@gmail.com>
2023-12-12 19:36:59 +0100

    Cleaning up

--- utils/model_evaluation.jl
+++
@@ -1,7 +0,0 @@
-module ModelEvaluation
-    function evaluate_models(data)
-        # Implement model evaluation using different approaches (ANN, SVM, decision trees, kNN)
-        # Perform cross-validation and evaluate models
-        # Display results and comparisons
-    end
-end
